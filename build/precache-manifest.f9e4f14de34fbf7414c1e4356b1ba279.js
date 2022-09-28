17b212e9153fdee2d3ecf152bcf58aacb9f50e1d
Kumar Sanu <101383013+kumarsanu2002@users.noreply.github.com>
2022-09-28 10:52:52 +0530

    Add files via upload

---
+++ build/precache-manifest.f9e4f14de34fbf7414c1e4356b1ba279.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dea5c10171b0d8e1b7fd2fbd15f3c330",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ef23f4b4bbc41b52053a",
+    "url": "/static/css/main.86e49d01.chunk.css"
+  },
+  {
+    "revision": "3ad6d1e49ce717001e7c",
+    "url": "/static/js/2.cff83314.chunk.js"
+  },
+  {
+    "revision": "f6bb6e7e06135325042ea16538284523",
+    "url": "/static/js/2.cff83314.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef23f4b4bbc41b52053a",
+    "url": "/static/js/main.acf65828.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
